d5b669c135e160f899bd6809281152dbf5218f4d
Mideny21 <30451596+Mideny21@users.noreply.github.com>
2021-05-14 16:24:25 +0300

    update

--- app.js
+++ app.js
@@ -14,10 +14,6 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
-});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -27,5 +23,30 @@ app.use((req, res, next) => {
 // 3) ROUTES
 app.use('/api/v1/business', businessRouter);
 app.use('/api/v1/users', userRouter);
+
+
+app.all('*', (req,res, next )=>{
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`
+  // }) 
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`)
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+})
+
+
+app.use((err, req, res, next) =>{
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  });
+});
 
 module.exports = app;
