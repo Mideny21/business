19b79d335e83983fa9ddac41ddd18c17d4e32aad
Mideny21 <30451596+Mideny21@users.noreply.github.com>
2021-05-20 12:16:35 +0300

    updates

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+process.on('uncaughtException', err => {
+  console.log(err.name, err.message); 
+  console.log('UNCAUGHT EXCEPTION SHUTTING DOWN')
+  
+    process.exit(1); // 0 success 1 uncaught exception
+  
+});
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
-
 
 mongoose.connect(DB, {
   useUnifiedTopology: true,
@@ -19,6 +26,15 @@ mongoose.connect(DB, {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message); 
+  console.log('UNHANDLED REJECTION')
+  server.close(() => {
+    process.exit(1); // 0 success 1 uncaught exception
+  });
+});
+
